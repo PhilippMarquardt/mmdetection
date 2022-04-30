907cb681773998b0a26309c33faf7a2522cba8ba
PhilippMarquardt <p.marquardt15@googlemail.com>
2022-05-01 00:03:59 +0200

    .

--- mmdet/datasets/pipelines/loading.py
+++ mmdet/datasets/pipelines/loading.py
@@ -299,7 +299,7 @@ class LoadAnnotations:
         Returns:
             numpy.ndarray: The decode bitmap mask of shape (img_h, img_w).
         """
-
+        print("xd")
         if isinstance(mask_ann, list):
             # polygon -- a single object might consist of multiple parts
             # we merge all parts into one mask rle code
